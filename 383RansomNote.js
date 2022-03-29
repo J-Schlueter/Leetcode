aaccd5a17e1f4a26f80ca44403a13f34cb965c41
Joshua Schlueter <contact@joshuaschlueter.com>
2022-03-29 15:23:56 -0500

    two sum solution

--- 383RansomNote.js
+++ 383RansomNote.js
@@ -18,11 +18,9 @@ Input: ransomNote = "aa", magazine = "aab"
 Output: true
 */
 
+// Discussed javascript solution. time 88ms, 44.7 MB memory. 
 var canConstruct = function(ransomNote, magazine) {
-    if (ransomNote.length > magazine.length) {
-        return false
-    };
-    
+
     const map = {};
     for(let letter of magazine) {
         if (!map[letter]) {
@@ -39,3 +37,47 @@ var canConstruct = function(ransomNote, magazine) {
     }
     return true;
 };
+
+// original solution. time 128ms, 45.4 MB memory. 40 mins+ to create.
+var makeNote = function(ransomNote, magazine) {
+
+    //map our magazine letter:count pairings
+    let magObj = new Object
+    for (letter of magazine){
+            // arr[1] = a, letter = a
+            // arr[2] = a, letter = a, arrObj[a] = 1
+            if (magObj[letter] !== undefined ){
+                magObj[letter]++
+            } else {
+                magObj[letter] = 1
+            };
+    };
+    //map target letter:count pairings
+    let tarObj = new Object
+    for (letter of ransomNote){
+            // arr[1] = a, letter = a
+            // arr[2] = a, letter = a, arrObj[a] = 1
+            if (tarObj[letter] !== undefined ){
+                tarObj[letter]++
+            } else {
+                tarObj[letter] = 1
+            };
+    };
+
+    //compare the two pairings
+    for (letter in tarObj) {
+        if (tarObj[letter] > magObj[letter]){
+            return false
+        } else if (magObj[letter] == undefined) {
+            return false
+        } else {
+            console.log(`sufficient ${letter}'s`)
+        };
+        
+    };
+    return true
+};
+
+console.log(canConstruct("a","b"));
+console.log(canConstruct("fihjjjjei","hjibagacbhadfaefdjaeaebgi"));
+
