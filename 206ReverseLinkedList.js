f6a8c46d11fc32610664df3dad47aa36324e936f
Joshua Schlueter <contact@joshuaschlueter.com>
2022-04-01 23:11:18 -0500

    reverse linked list

---
+++ 206ReverseLinkedList.js
@@ -0,0 +1,15 @@
+//given the head of a singly linked list, reverse the list and RETURN THE REVERSED LIST.
+//translate => iterate throught the orginal list, and building the new list from end to start.
+
+/*
+Definition for singly-linked list.
+     function ListNode(val, next) {
+     this.val = (val===undefined ? 0 : val)
+      this.next = (next===undefined ? null : next)
+    }
+*/
+
+var reverseList = function(head) {
+    let reversedList = {};
+    for (i )
+}
