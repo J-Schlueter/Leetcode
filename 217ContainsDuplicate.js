7c4590d324264d6eec5bde14633216bf3aa8f614
Joshua Schlueter <contact@joshuaschlueter.com>
2022-04-04 15:46:57 -0500

    refactor

--- 217ContainsDuplicate.js
+++ 217ContainsDuplicate.js
@@ -11,12 +11,24 @@
 // Input: nums = [1,1,1,3,3,4,3,2,4,2]
 // Output: true
 
-var containsDuplicate = function(nums) {
-    //sort the array O(n log n)
-   nums.sort((a,b)=>{return b-a});
-   //loop through and compare each number to the previous number, if the same return true O(n)
-   for (i =0; i < nums.length; i++){
-       if (i > 0 && nums[i-1] === nums[i]) return true;
-   };
-   return false;
-};
+// var containsDuplicate = function(nums) {
+//     //sort the array O(n log n)
+//    nums.sort((a,b)=>{return b-a});
+//    //loop through and compare each number to the previous number, if the same return true O(n)
+//    for (i =0; i < nums.length; i++){
+//        if (i > 0 && nums[i-1] === nums[i]) return true;
+//    };
+//    return false;
+// };
+
+var containsDuplicateV2 = function(nums) {
+    let collection = new Map();
+
+    for (let i = 0; i < nums.length; i++){
+        if (collection.has(nums[i])){
+            return true;
+        } 
+        collection.set(nums[i],true);
+    }
+    return false;
+}
