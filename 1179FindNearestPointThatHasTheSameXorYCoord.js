87420a4c81fdb9464fe72ab08b2a39c2e4d962d1
Joshua Schlueter <contact@joshuaschlueter.com>
2022-04-06 17:48:44 -0500

    notes change... again

--- 1179FindNearestPointThatHasTheSameXorYCoord.js
+++ 1179FindNearestPointThatHasTheSameXorYCoord.js
@@ -1,10 +1,28 @@
-let testX = 71, testY = 13, testPoints = [[32,10],[3,7],[16,65],[57,68],[25,14],[82,70],[80,2],[34,54],[62,77],[46,33],[1,68],[83,77],[56,29],[21,66],[79,81],[15,50],[44,36],[67,79],[61,59],[59,55],[46,21],[66,11],[55,33],[48,54],[41,83],[21,2],[81,27],[24,35],[50,83],[60,27],[45,38],[43,36],[15,34],[33,17],[69,31],[13,48],[71,66],[53,62],[29,12],[22,30],[24,40],[13,11],[22,17],[68,46],[17,37]]
-// testPoints = [[3,4]]
-// testPoints = [[2,3]]
-console.log(nearestValidPoint(testX,testY,testPoints))
 
-//time to complete 1hr+
+/*  on whiteboard
+step 0) REPHRASE, given a target x,y and an array of points, return the index of the closest point.
+step 1) define the answer = valid points, lowest manhattan, lowest index
+step 2) pseudocode
+-search points for valid coordinates, addind to seperate array
+-calculate manhattan point for each valid cord
+-if manPoints are tied compare points index
+-return lowest points index or -1 if no valid point index exists
+    on IDE
+step 3) code by each pseudocode step, testing each feature
+step 4) adaptation
+    started by creating seperate array of manhattanPoints but decided to keep track of lowest point and check at time of calculation
+    
+
+    I was passing 98/101 tests on leetcode, so I had to troubleshoot for a while. The issue was that I had set lowestManhattan = points.length, but some of the test cases had really large manhattan points
+    so I changed lowestManhattan to start = Infinity
+step 5) refactor
+    got rid of validPoints array are lowestIndex can be start at -1 and update for at the first valid coordinate
+
+//time to complete to first pass 30 mins, to last refactor 1:30
 // runtime O(n) space O(1)
+*/
+
+
 function nearestValidPoint(x,y,points){
     // let validPoints = [];
     let lowestManhattan = Infinity;
@@ -19,7 +37,7 @@ function nearestValidPoint(x,y,points){
             }
         }
     }
-    //#ALTERNATIVE#
+    //#Slight Refactor#
     // for (let cord of points){
     //     if (cord[0] === x || cord[1] === y){
     //         validPoints.push(cord)
@@ -41,25 +59,12 @@ function nearestValidPoint(x,y,points){
 };
 
 
-/*  on whiteboard
-step 0) REPHRASE, given a target x,y and an array of points, return the index of the closest point.
-step 1) define the answer = valid points, lowest manhattan, lowest index
-step 2) pseudocode
--search points for valid coordinates, addind to seperate array
--calculate manhattan point for each valid cord
--if manPoints are tied compare points index
--return lowest points index or -1 if no valid point index exists
-    on IDE
-step 3) code by each pseudocode step, testing each feature
-step 4) adaptation
-    started by creating seperate array of manhattanPoints but decided to keep track of lowest point and check at time of calculation
-    
+let testX = 71, testY = 13, testPoints = [[32,10],[3,7],[16,65],[57,68],[25,14],[82,70],[80,2],[34,54],[62,77],[46,33],[1,68],[83,77],[56,29],[21,66],[79,81],[15,50],[44,36],[67,79],[61,59],[59,55],[46,21],[66,11],[55,33],[48,54],[41,83],[21,2],[81,27],[24,35],[50,83],[60,27],[45,38],[43,36],[15,34],[33,17],[69,31],[13,48],[71,66],[53,62],[29,12],[22,30],[24,40],[13,11],[22,17],[68,46],[17,37]]
+// testPoints = [[3,4]]
+// testPoints = [[2,3]]
+console.log(nearestValidPoint(testX,testY,testPoints))
 
-    I was passing 98/101 tests on leetcode, so I had to troubleshoot for a while. The issue was that I had set lowestManhattan = points.length, but some of the test cases had really large manhattan points
-    so I changed lowestManhattan to start = Infinity
-step 5) refactor
-    got rid of validPoints array are lowestIndex can be start at -1 and update for at the first valid coordinate
-*/
+
 
 function refactor(x, y, points) {
     let bestManhattan = Infinity
